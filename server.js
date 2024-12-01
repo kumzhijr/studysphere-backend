a11920b9704a38920a363dd670b0375a331e43a2
kumzhijr <ponsar.kumzhi@gmail.com>
2024-12-01 08:24:03 +0400

    Added descriptive comment

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use((req, res, next) => {
   next();
 });
 
-
+// Get all lessons
 app.get('/api/lessons/:id', async (req, res) => {
   const lessonId = parseInt(req.params.id, 10); // Ensure the ID is treated as a number
   try {
